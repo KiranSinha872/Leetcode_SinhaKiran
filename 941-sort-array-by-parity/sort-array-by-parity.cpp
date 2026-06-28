2c1b2993aa333d82fb82cee2689107190bc71b4b
KiranSinha <sinhakiran872@gmail.com>
2026-06-28 12:16:24 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (79.69%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -5,15 +5,15 @@ public:
         int right = nums.size() - 1;
         
         while (left < right) {
-            // Find odd element on the left and even element on the right to swap
+            
             if (nums[left] % 2 != 0 && nums[right] % 2 == 0) {
                 swap(nums[left], nums[right]);
             }
-            // Advance left if it's already even
+        
             if (nums[left] % 2 == 0) {
                 left++;
             }
-            // Decrement right if it's already odd
+           
             if (nums[right] % 2 != 0) {
                 right--;
             }
