c8a099c1bcae8f1363d539579aad80143f75e4dd
KiranSinha <sinhakiran872@gmail.com>
2026-06-28 12:07:05 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (80.88%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> expected = heights;
+        vector<int> expected = heights;     
         sort(expected.begin(), expected.end());       
-        int mismatchCount = 0;     
+        int mismatchCount = 0;
         for (int i = 0; i < heights.size(); i++) {
             if (heights[i] != expected[i]) {
                 mismatchCount++;
             }
-        }      
+        }
+        
         return mismatchCount;
     }
 };
