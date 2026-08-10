e0b2dd114d8aa26c0748a7b4e44cc6801656d3a5
KiranSinha <sinhakiran872@gmail.com>
2026-08-10 18:51:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.11%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -9,9 +9,11 @@ public:
                 st.push(str[i]);
             }//closing
             else{
+                //if stack is already empty so only closing bracket are there
                 if(st.size()==0){
                     return false;
                 }
+                //matching
                 if( (st.top()=='(' && str[i]==')' )||
                     (st.top()=='{' && str[i]=='}' )||
                     (st.top()=='[' && str[i]==']' )){
